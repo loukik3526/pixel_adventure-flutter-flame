0d94dc05f35250181c8a8511905c24b17a2390ad
Loukik Langhi <loukiklanghi21@gmail.com>
2025-10-16 19:02:13 +0530

    this is something updated

---
+++ lib/components/fruit.dart
@@ -0,0 +1,6 @@
+// import 'package:flame/components.dart';
+//
+// class Fruit extends SpriteAnimationComponent
+// {
+//   Fruit({position,sizeh})
+// }
--- lib/components/level.dart
+++ lib/components/level.dart
@@ -1,10 +1,12 @@
 import 'dart:async';
 import 'package:flame/components.dart';
 import 'package:flame_tiled/flame_tiled.dart';
+import 'package:pixel_adventure/components/background_tile.dart';
 import 'package:pixel_adventure/components/collision_block.dart';
 import 'package:pixel_adventure/components/player.dart';
+import 'package:pixel_adventure/pixel_adventure.dart';
 
-class Level extends World
+class Level extends World with HasGameRef<PixelAdventure>
 {
   final String levelName;
   final Player player;
@@ -19,51 +21,86 @@ class Level extends World
 
     add(level);
 
+    _scrollingBackground();
+    _spawningObjects();
+    _addCollisions();
+
+    return super.onLoad();
+  }
+
+  void _scrollingBackground() {
+    final backgroundLayer = level.tileMap.getLayer('Background');
+    const tileSize = 64;
+    final numTilesX = (game.size.x / tileSize).floor();
+    final numTilesY = (game.size.y / tileSize).floor();
+
+    if(backgroundLayer != null)
+      {
+        final backgroundColor = backgroundLayer.properties.getValue('BackgroundColor');
+
+        for(double y = 0; y < game.size.y / numTilesY;y++)
+          {
+            for(double x = 0; x < numTilesX; x++)
+              {
+                final backgroundTile =BackgroundTile(color: backgroundColor != null ? backgroundColor : 'Gray',
+                    position: Vector2(x * tileSize , y * tileSize - tileSize));
+                add(backgroundTile);
+              }
+          }
+
+
+      }
+  }
+
+  void _spawningObjects() {
     final spawnPointsLayer = level.tileMap.getLayer<ObjectGroup>('SpawnPoints');
 
     if(spawnPointsLayer != null )
+    {
+      for(final spawnPoint in spawnPointsLayer.objects)
       {
-        for(final spawnPoint in spawnPointsLayer.objects)
+        switch(spawnPoint.class_)
         {
-          switch(spawnPoint.class_)
-          {
-            case'Player':
-              player.position = Vector2(spawnPoint.x, spawnPoint.y);
-              add(player);
-              break;
-            default:
-          }
+          case'Player':
+            player.position = Vector2(spawnPoint.x, spawnPoint.y);
+            add(player);
+            break;
+          case'Fruit':
+
+          default:
         }
       }
+    }
+  }
 
+  void _addCollisions() {
     final collisionsLayer = level.tileMap.getLayer<ObjectGroup>('Collisions');
 
     if(collisionsLayer != null)
+    {
+      for(final collision in collisionsLayer.objects)
       {
-        for(final collision in collisionsLayer.objects)
-          {
-            switch (collision.class_)
-                {
-              case 'Platform':
-                final platform = CollisionBlock(
-                  position: Vector2(collision.x, collision.y),
-                  size: Vector2(collision.width, collision.height),
-                  isPlatform: true,
-                );
-                collisionBlocks.add(platform);
-                add(platform);
-                break;
-              default:
-                final block = CollisionBlock(
-                  position: Vector2(collision.x, collision.y),
-                  size: Vector2(collision.width, collision.height),
-                );
-                collisionBlocks.add(block);
-                add(block);
-            }
-          }
+        switch (collision.class_)
+        {
+          case 'Platform':
+            final platform = CollisionBlock(
+              position: Vector2(collision.x, collision.y),
+              size: Vector2(collision.width, collision.height),
+              isPlatform: true,
+            );
+            collisionBlocks.add(platform);
+            add(platform);
+            break;
+          default:
+            final block = CollisionBlock(
+              position: Vector2(collision.x, collision.y),
+              size: Vector2(collision.width, collision.height),
+            );
+            collisionBlocks.add(block);
+            add(block);
+        }
       }
-      player.collisionBlocks = collisionBlocks;
-    return super.onLoad();
+    }
+    player.collisionBlocks = collisionBlocks;
   }
 }
